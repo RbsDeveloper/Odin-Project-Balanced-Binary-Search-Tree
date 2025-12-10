87712940c73de7a90a914a927d991365373462c9
Raul <developer.rbs@gmail.com>
2025-12-11 01:00:08 +0200

    Feat: add driver script to demo BST operations and balancing

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 import { Bst } from "./tree.js";
 import { prettyPrint } from "./utils/prettyPrint.js";
 
-const initialData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-const tree = new Bst(initialData);
+const randomArr = Array.from({length: 15}, ()=> Math.floor(Math.random() * 100));
 
-console.log(tree);
+const tree = new Bst(randomArr);
 
-//prettyPrint(tree.root);
+prettyPrint(tree.root);
 
-//tree.insert(6);
+console.log(tree.isBalanced());
 
-//prettyPrint(tree.root);
+tree.levelOrderForEach((val)=>console.log(val))
+tree.inOrderForEach((val)=>console.log(val))
+tree.postOrderForEach((val)=>console.log(val))
+tree.preOrderForEach((val)=>console.log(val))
 
-//tree.delete(324);
+tree.insert(103)
+prettyPrint(tree.root);
+tree.insert(107)
+prettyPrint(tree.root);
+tree.insert(120)
+prettyPrint(tree.root);
+tree.insert(124)
+prettyPrint(tree.root);
+console.log(tree.isBalanced());
 
-//prettyPrint(tree.root);
-
-//console.log(tree.find(6));
-
-//console.log(tree.postOrderForEach(()=>{console.log('hi')}))
-
-// console.log(tree.height(67));
-// console.log(tree.depth(2));
-
-tree.insert(325);
-tree.insert(330);
-
-prettyPrint(tree.root)
-
-console.log(tree.isBalanced())
+tree.levelOrderForEach((val)=>console.log(val))
+tree.inOrderForEach((val)=>console.log(val))
+tree.postOrderForEach((val)=>console.log(val))
+tree.preOrderForEach((val)=>console.log(val))
